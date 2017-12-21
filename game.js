72a69d26945ff962fc6e1b4388fd82c19e567326
cecespana <32506449+cecespana@users.noreply.github.com>
2017-12-21 18:06:08 +0100

    Animation balle
    Creation de fonction pour animer la balle.
    Utilisation de requestAnimationFrame().
    
    A faire : Gerer collision pour que la balle ne sorte pas du terrain.

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 
 
-// pour exposer la position (x,y) creation d'une classe vecteur
-class Vecteur{
+// pour exposer la position (x,y) creation d'une classe Coordonnee
+class Coordonnee{
 	constructor(x = 0, y =0){
 		this.x = x;
 		this.y = y;
@@ -12,15 +12,15 @@ class Vecteur{
 
 class Rectangle{
 	constructor(largeur,hauteur){
-		this.position = new Vecteur;
-		this.taille = new Vecteur(largeur,hauteur);
+		this.position = new Coordonnee;
+		this.taille = new Coordonnee(largeur,hauteur);
 	}
 }
 	
 class Balle extends Rectangle{
 	constructor(){
 		super(20,20);
-		this.vitesse = new Vecteur;
+		this.vitesse = new Coordonnee;
 	}
 	
 }
@@ -30,12 +30,45 @@ const canvas  = document.getElementById("gameCanvas"); // acces au canvas
 const context = canvas.getContext("2d"); // contexte en 2d
 
 const balleGame = new Balle;
-console.log(balleGame);
+console.log(balleGame); // balleGame est bien un objet heritant de rectangle
 
-// mise en place du terrain de jeu
-context.fillStyle = '#33919E';
-context.fillRect(0,0,canvas.width,canvas.height);
+// mettre la balle au millieu de terrain
 
-// creation de la balle
-context.fillStyle = '#e50000';
-context.fillRect(canvas.width/2,canvas.height/2,20,20);
+balleGame.position.x = (canvas.width/2);
+balleGame.position.y = (canvas.height/2);
+
+// vitesse de la balle 
+
+balleGame.vitesse.x = 100;
+balleGame.vitesse.y = 100;
+
+let ancienPosition;
+
+function rappeler(milliseconde){
+	if(ancienPosition){
+		animerBalle((milliseconde-ancienPosition)/1000)
+	}
+	ancienPosition = milliseconde;
+	requestAnimationFrame(rappeler);
+}
+
+
+// ANIMATION DE LA Balle
+// modifier la position de la balle en fonction du temps
+function animerBalle(temps){
+	balleGame.position.x += balleGame.vitesse.x * temps; // balle va a droite
+	balleGame.position.y += balleGame.vitesse.y * temps; // balle va en bas
+	
+	// mise en place du terrain de jeu
+	context.fillStyle = '#33919E';
+	context.fillRect(0,0,canvas.width,canvas.height);
+
+	// creation de la balle
+	context.fillStyle = '#e50000';
+	context.fillRect(balleGame.position.x,balleGame.position.y,balleGame.taille.x,balleGame.taille.y);
+	
+}
+
+rappeler();
+
+
